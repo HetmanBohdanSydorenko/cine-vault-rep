e409527e2d0f4b1d5f19b3ae06c3928874672d6b
Deutsche Telekom Corp Git <bohdanchik_munich@telekom.de>
2025-02-25 22:34:03 +0200

    Complete topic-6-versioning

--- src/CineVault.API/Controllers/AppInfoController.cs
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CineVault.API.Controllers;
 
-[Route("api/[controller]/[action]")]
+[Route("api/v{v:apiVersion}/[controller]/[action]")]
+[ApiVersion(1)]
+[ApiVersion(2)]
 public class AppInfoController : ControllerBase
 {
     [HttpGet]
     public string GetEnvironment(IWebHostEnvironment environment)
     {
         return environment.EnvironmentName;
+    }
+
+    [MapToApiVersion(1)]
+    [HttpGet]
+    public string GetCodeInFirstVersion()
+    {
+        string description = "The version of application: ";
+        return description + 2021;
+    }
+
+    [MapToApiVersion(2)]
+    [HttpGet]
+    public string GetCodeInSecondVersion()
+    {
+        string description = "The version of this application will be ";
+        return description + 2025;
     }
 }
