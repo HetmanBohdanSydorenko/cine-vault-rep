37794c8f0ca084aa9b7c91b5dda2a9f373c2a1a7
Deutsche Telekom Corp Git <bohdanchik_munich@telekom.de>
2025-02-25 22:04:10 +0200

    Complete topic-5-environments

---
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CineVault.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class AppInfoController : ControllerBase
+{
+    [HttpGet]
+    public string GetEnvironment(IWebHostEnvironment environment)
+    {
+        return environment.EnvironmentName;
+    }
+}
